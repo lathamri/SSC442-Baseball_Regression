8acdb08899ac3156023643c7b81bfd32064bcd20
Riley Latham <lathamri@msu.edu>
2020-01-14 20:48:58 -0500

    Exercise 1 Lab 1 Complete

--- Lab_Working_Files/SSC442_Lab_1.R
+++ Lab_Working_Files/SSC442_Lab_1.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(ggplot2)
 library(gapminder)
+library(scales)
 
 ggplot2::mpg
 # typing geom_ will show all possible options of plots from ggplot2
 
 # generates gapminder data set
-library(gapminder)
 head(gapminder)
 # binds our variables of interest, gpdpercap and life exp
 p = ggplot(data=gapminder,
@@ -36,11 +36,130 @@ r + geom_point()
 # us anyting intersting about our data. Perhaps a count of cars fitting into
 # these categories would be more insightful.
 
-q + geom_point()
-
 p + geom_point() + geom_smooth()
 
 ?geom_smooth()
+
+# Running different methods and aes for smooth
+p + geom_point() + geom_smooth(method='gam')
+p + geom_point() + geom_smooth(method='auto')
+# this is the best for small data sets but is computationally inefficient
+p + geom_point() + geom_smooth(method='loess', colour='red')
+
+# log scaling to spread the data better
+p + geom_point() + 
+  geom_smooth(method='lm') + 
+  scale_x_log10(labels = scales::dollar)
+
+# scale_x_log10 description:
+# The data appears to be logarithmic because of the grouping so we apply a log
+# scale to the data to better visualize it. What ends up happening is instead 
+# of using a linear scale we change the x axis to use a logarithmic scale 
+# so that our data appears more linear without loss of generality.
+
+?dollar
+?scales
+
+# This call changes our labels on the x-axis to dollars instead of the current
+# scientific notation that comes with scale_x_log10.
+# Other label changes can be applied according to the scales documentation.
+# Some examples include byte size, dates, percents and more.
+
+# Redefines p variables
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp, color = 'yellow'))
+p + geom_point() + scale_x_log10()
+
+# Our mapping aes changes the color value for the data p but this is
+# then looked as a data descriptor (obviously it's not). Rhe color
+# is determined in the geom calls so we have to set the color value there.
+# Interestingly the color call in mapping defines our data label as whatever you set
+# the value to be, in this case yellow but any string works.
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point(colour='yellow') + scale_x_log10()
+
+# The above changes fix the issue and craft yellow data points.
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point() + 
+  geom_smooth(color = "orange", se = FALSE, size = 8, method = "lm") + 
+  scale_x_log10()
+
+# Color sets our approx. line to orange, se=False turns on conf. intervals,
+# size sets the thickness of the line to 8 and method creates
+# a linear model of the data.
+
+p + geom_point(alpha = 0.3) +
+  geom_smooth(method = "gam") +
+  scale_x_log10(labels = scales::dollar) +
+  labs(x = "GDP Per Capita", y = "Life Expectancy in Years",
+       title = "Economic Growth and Life Expectancy",
+       subtitle = "Data Points are country-years",
+       caption = "Source: Gapminder")
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp, color = continent, fill = continent))
+
+p + geom_point()
+p + geom_point() + scale_x_log10(labels = dollar)
+p + geom_point() + scale_x_log10(labels = dollar) + geom_smooth()
+
+# fill=continent is sorting the data by continent and applying unique colors to
+# each point in the data and the smoothing lines as well.
+# Personally I think it's a little messy and maybe silly to have the data share
+# exact colors and slightly more translucent confidence intervals. I would prefer
+# to change the alpha values of our dots to better allow us to see the trend lines.
+# I've plotted this below but it still needs tweaks to capture the best 
+# interpretation for the dataset.
+
+p + geom_point(alpha=0.3) + scale_x_log10(labels = dollar) + geom_smooth()
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point(mapping = aes(color = continent)) + geom_smooth() + scale_x_log10()
+
+# This code is giving us one regression line because of the mapping to
+# color continent being assigned in the geom_point call. Coloring by
+# continenet is a direct effect of the data and must be applied in the data
+# section when we assign data to our p object.
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point(mapping = aes(color = continent)) +
+  geom_smooth(mapping = aes(color = continent, fill = continent)) +
+  scale_x_log10() +
+  geom_smooth(mapping = aes(color = continent), method = "gam")
+
+# Instead of assiging aes mappings for each geom we're better off assigning 
+# them all at once in our first point so they stick to the data rather 
+# than the geom. The following function demonstrates the difference.
+# It's a much more readable function with the same graph.
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp, color=continent, fill=continent))
+p + geom_point() +
+  geom_smooth() +
+  scale_x_log10() +
+  geom_smooth(method = "gam")
+
+##EXERCISE 2##
+
+library(readr)
+
+urlfile = 'https://raw.githubusercontent.com/MSUDataAnalytics/SSC442/master/assets/bank.csv'
+bank = read_csv(url(urlfile))
+head(bank)
+
+balance_duration = ggplot(data=bank,
+                          mapping=aes(x=job, y=balance, color = education))
+
+balance_duration + geom_jitter() +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+
 
 
 
